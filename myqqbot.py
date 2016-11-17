ceb086af26edcb33d03d0ba85131a3f86e31cc3c
aong <yxwzaxns@gmail.com>
2016-11-17 20:18:43 +0800

    new myqqbot

--- myqqbot.py
+++ myqqbot.py
@@ -2,46 +2,47 @@
 # -*- coding: utf-8 -*-
 
 from qqbot import QQBot
-
+import re
 
 class MyQQBot(QQBot):
     def onPollComplete(self, msgType, from_uin, buddy_uin, message):
         reply = ''
         message = message.split()
-        if msg[0] == 'command:':
-            if re.search('ss',msg[1]):
-                 reply = requests.get('https://ss.aong.cn', verify = 'ss.pem').text
-            elif message[1] == '-help':
-                reply = '欢迎使用QQBot，使用方法：\n' + \
-                        '\t-help\n' + \
-                        '\t-list {buddy|group|discuss}\n' + \
-                        '\t-send {buddy|group|discuss} {buddy_qq|group_qq|discuss_uin} message[1]\n' + \
-                        '\t-refetch\n' + \
-                        '\t-stop\n'
-            elif message[1][:6] == '-list ':
-                reply = getattr(self, message[1][6:].strip()+'Str', '')
-            elif message[1][:6] == '-send ':
-                args = message[1][6:].split(' ', 2)
-                if len(args) == 3 and args[1].isdigit() and args[0] in ['buddy', 'group', 'discuss']:
-                    n = int(args[1])
-                    try:
-                        if args[0] == 'buddy':
-                            uin = self.buddiesDictQ[n]['uin']
-                        elif args[0] == 'group':
-                            uin = self.groupsDictQ[n]['uin']
+        if not message:
+            if message[0] == 'command:':
+                if re.search('ss',message[1]):
+                     reply = requests.get('https://ss.aong.cn', verify = 'ss.pem').text
+                elif message[1] == '-help':
+                    reply = '欢迎使用QQBot，使用方法：\n' + \
+                            '\t-help\n' + \
+                            '\t-list {buddy|group|discuss}\n' + \
+                            '\t-send {buddy|group|discuss} {buddy_qq|group_qq|discuss_uin} message[1]\n' + \
+                            '\t-refetch\n' + \
+                            '\t-stop\n'
+                elif message[1][:6] == '-list ':
+                    reply = getattr(self, message[1][6:].strip()+'Str', '')
+                elif message[1][:6] == '-send ':
+                    args = message[1][6:].split(' ', 2)
+                    if len(args) == 3 and args[1].isdigit() and args[0] in ['buddy', 'group', 'discuss']:
+                        n = int(args[1])
+                        try:
+                            if args[0] == 'buddy':
+                                uin = self.buddiesDictQ[n]['uin']
+                            elif args[0] == 'group':
+                                uin = self.groupsDictQ[n]['uin']
+                            else:
+                                uin = self.discussesDict[n]['uin']
+                        except KeyError:
+                            reply = '接收者账号错误'
                         else:
-                            uin = self.discussesDict[n]['uin']
-                    except KeyError:
-                        reply = '接收者账号错误'
-                    else:
-                        self.send(args[0], uin, args[2].strip())
-                        reply = '消息发送成功'
-            elif message[1] == '-refetch':
-                self.refetch()
-                reply = '重新获取 好友/群/讨论组 成功'
-            elif message[1] == '-stop':
-                reply = 'QQBot已关闭'
-                self.stopped = True
+                            self.send(args[0], uin, args[2].strip())
+                            reply = '消息发送成功'
+                elif message[1] == '-refetch':
+                    self.refetch()
+                    reply = '重新获取 好友/群/讨论组 成功'
+                elif message[1] == '-stop':
+                    reply = 'QQBot已关闭'
+                    self.stopped = True
         self.send(msgType, from_uin, reply)
 
 myqqbot = MyQQBot()
